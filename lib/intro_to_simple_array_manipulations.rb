86a9937dc0a32c1d1157323677baecbfb93ed5f0
nick repasy <nrepasy@gmail.com>
2020-11-26 04:45:44 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,4 +6,10 @@ def using_push(colors_in_the_rainbow, next_color)
   colors_in_the_rainbow.push(next_color)
 end
 
- 
+bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+new_neighborhood = "Staten Island"
+
+def using_unshift(bouroughs_in_nyc , new_neighborhood)
+  bouroughs_in_nyc.unshift(new_neighborhood)
+end
+  
